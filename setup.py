abbc1b3107e6da6f6da3f9baad26d1f59df61eff
Fantomas42 <fantomas42@gmail.com>
2010-02-22 14:38:46 +0100

    adding project files

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='emencia.django.activity',
+      version=version,
+      description='Logging activities on your models for displaying site activity in your Django projects.',
+      long_description=open('README.rst').read() + '\n' +
+                       open(os.path.join('docs', 'HISTORY.txt')).read(),
+      keywords='django, activity, log, events',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'License :: OSI Approved :: BSD License',
+          ],
+
+      author='Fantomas42',
+      author_email='fantomas42@gmail.com',
+      url='http://emencia.fr',
+      
+      license='BSD License',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['emencia', 'emencia.django'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
