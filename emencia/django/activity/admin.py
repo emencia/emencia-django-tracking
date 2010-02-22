47840c512b67bc5a94b4f601d3c6c7c098feb86a
Fantomas42 <fantomas42@gmail.com>
2010-02-22 15:18:29 +0100

    adding prototype code

--- emencia/django/activity/admin.py
+++ emencia/django/activity/admin.py
@@ -1,3 +1,32 @@
 """Admin for emencia.django.activity"""
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _
+
+from emencia.django.activity.models import Activity
+
+class ActivityAdmin(admin.ModelAdmin):
+    date_hierarchy = 'creation_date'
+    search_fields = ('title', 'description')
+    list_display = ('title', 'action', 'related_object_admin', 'creation_date')
+    list_filter = ('action', 'creation_date')
+    fieldsets = ((None, {'fields': ('title', 'description', 'action')}),
+                 (_('Advanced'), {'fields': ('object_id', 'content_type'),
+                                  'classes': ('collapse',)}),)
+    actions_on_top = False
+    actions_on_bottom = True
+
+    def related_object_admin(self, activity):
+        """Display link to related object's admin"""
+        if activity.content_type and activity.object_id:
+            admin_url = reverse('admin:%s_%s_change' % (activity.content_type.app_label,
+                                                        activity.content_type.model),
+                                args=(activity.object_id,))
+            return '%s: <a href="%s">%s</a>' % (activity.content_type.model.capitalize(),
+                                                admin_url,
+                                                activity.content_object.__unicode__())
+        return _('No relative object')
+    related_object_admin.allow_tags = True
+    related_object_admin.short_description = _('Related object')
+
+admin.site.register(Activity, ActivityAdmin)
--- emencia/django/activity/models.py
+++ emencia/django/activity/models.py
@@ -1,4 +1,30 @@
 """Models for emencia.django.activity"""
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+
+INSERT = 1
+CHANGE = 2
+
+class Activity(models.Model):
+    ACTION_CHOICES = ((INSERT, _('insertion')),
+                      (CHANGE, _('changement')),)
+
+    action = models.IntegerField(_('action type'), choices=ACTION_CHOICES)
+
+    title = models.CharField(_('title'), max_length=250)
+    description = models.TextField(_('description'), blank=True)
+
+    content_type = models.ForeignKey(ContentType, blank=True, null=True)
+    object_id = models.PositiveIntegerField(blank=True, null=True)
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    
+    creation_date = models.DateTimeField(_('creation date'), auto_now_add=True)
+
+    class Meta:
+        ordering = ('creation_date',)
+        verbose_name = _('activity')
+        verbose_name_plural = _('activities')
+
 
